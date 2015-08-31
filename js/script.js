e92fed929e198f699c1ee7bead7f85cd2170a354
kpayak <payak.keyur@gmail.com>
2015-08-31 08:35:37 -0700

    updated for mobile

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
         if ($(document).scrollTop() > 400) {
             $('.navbarRow').addClass('sticky');
+            $('.mobileNavBar').addClass('sticky');
         }
         
         else {
@@ -26,13 +27,22 @@ $(document).ready(function(){
 
 $(".materialCard")
 .mouseenter(function () {
-    console.log("detected hover");
     //var card = $(this).parent().parent();      
     $(this).addClass('active');
 })
 .mouseleave(function(){
     console.log("leaving");
     $(this).removeClass('active');
+});
+
+// Script to mark sections on vertical scroll
+
+$(".whatWeDo").mouseenter ( function() {
+    console.log("entered what we do");
+});
+
+$(".whatWeDo").mouseleave ( function() {
+    console.log("leaving what we do");
 });
 
 
